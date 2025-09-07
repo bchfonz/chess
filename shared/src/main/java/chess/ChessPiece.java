c296b28918b17b571f9f330b5d800ded02dea5a1
Benjamin Fonbuena <bchfonz@gmail.com>
2025-09-06 18:49:49 -0600

    Bishop moves working

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -3,6 +3,7 @@ package chess;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Represents a single chess piece
@@ -147,6 +148,53 @@ public class ChessPiece {
                 {0,0}
             };
         }
+
+        if(piece.type == PieceType.BISHOP || piece.type == PieceType.ROOK || piece.type == PieceType.QUEEN){
+            return allOtherMoves(board, myPosition, directions);
+        }
+
         return List.of();
+    }
+
+    private Collection<ChessMove> allOtherMoves(ChessBoard board, ChessPosition myPosition, int[][] directions){
+        Collection<ChessMove> validMoves = new ArrayList<>();
+        ChessPiece myPiece = board.getPiece(myPosition);
+        for(int[] direction : directions){
+            int curRow = myPosition.getRow();
+            int curCol = myPosition.getColumn();
+            curRow += direction[0];
+            curCol += direction[1];
+            while(curRow <= 8 && curRow > 0 && curCol <= 8 && curCol > 0){
+                ChessPosition curPosition = new ChessPosition(curRow, curCol);
+                ChessPiece curSquare = board.getPiece(curPosition);
+                if(curSquare == null){
+                    validMoves.add(new ChessMove(myPosition, curPosition, null));
+                }
+                else if(curSquare.pieceColor != myPiece.pieceColor){
+                    validMoves.add(new ChessMove(myPosition, curPosition, null));
+                    break;
+                }
+                else if(curSquare.pieceColor == myPiece.pieceColor){
+                    break;
+                }
+                curRow += direction[0];
+                curCol += direction[1];
+            }
+        }
+        return validMoves;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessPiece piece = (ChessPiece) o;
+        return pieceColor == piece.pieceColor && type == piece.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pieceColor, type);
     }
 }
