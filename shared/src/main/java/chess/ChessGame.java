1e3dbe2ca3a31a08001c816b45b496a575587c11
Benjamin Fonbuena <bchfonz@gmail.com>
2025-09-15 11:50:49 -0600

    Implemented some getters and setters in ChessGame and added Phase 1 tests

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -10,6 +10,8 @@ import java.util.Collection;
  */
 public class ChessGame {
 
+    private ChessBoard gameBoard = new ChessBoard();
+    private TeamColor curPlayer = TeamColor.WHITE;
     public ChessGame() {
 
     }
@@ -18,7 +20,7 @@ public class ChessGame {
      * @return Which team's turn it is
      */
     public TeamColor getTeamTurn() {
-        throw new RuntimeException("Not implemented");
+        return curPlayer;
     }
 
     /**
@@ -27,7 +29,7 @@ public class ChessGame {
      * @param team the team whose turn it is
      */
     public void setTeamTurn(TeamColor team) {
-        throw new RuntimeException("Not implemented");
+        curPlayer = team;
     }
 
     /**
@@ -105,6 +107,6 @@ public class ChessGame {
      * @return the chessboard
      */
     public ChessBoard getBoard() {
-        throw new RuntimeException("Not implemented");
+        return gameBoard;
     }
 }
