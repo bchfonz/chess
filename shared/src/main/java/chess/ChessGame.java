7fd5101b4191f4c2628b41b26e55ddeb68c7b5f0
Benjamin Fonbuena <bchfonz@gmail.com>
2025-09-19 20:09:18 -0600

    Added equals and hashcode to ChessGame

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -2,6 +2,7 @@ package chess;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Objects;
 
 /**
  * For a class that can manage a chess game, making moves on a board
@@ -259,5 +260,19 @@ public class ChessGame {
      */
     public ChessBoard getBoard() {
         return gameBoard;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessGame chessGame = (ChessGame) o;
+        return Objects.equals(gameBoard, chessGame.gameBoard) && curPlayer == chessGame.curPlayer;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(gameBoard, curPlayer);
     }
 }
