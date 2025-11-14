97936b2214f28ea953d1c123d8d69fcefaec03b0
Benjamin Fonbuena <bchfonz@gmail.com>
2025-11-14 00:15:26 -0700

    Setup ServerFacadeTests and got validRegister test working

--- client/src/main/java/server/ServerFacade.java
+++ client/src/main/java/server/ServerFacade.java
@@ -2,44 +2,53 @@ package server;
 
 import com.google.gson.Gson;
 import exception.ResponseException;
-import model.*;
+import service.*;
 
-import java.net.*;
-import java.net.http.*;
-import java.net.http.HttpRequest.BodyPublisher;
-import java.net.http.HttpRequest.BodyPublishers;
-import java.net.http.HttpResponse.BodyHandlers;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class ServerFacade {
     private final HttpClient client = HttpClient.newHttpClient();
     private final String serverUrl;
 
-    public ServerFacade(String url) {
+
+    public ServerFacade(String url){
         serverUrl = url;
     }
 
-    public Pet addPet(Pet pet) throws ResponseException {
-        var request = buildRequest("POST", "/pet", pet);
+    public RegAndLoginResult register(RegisterRequest regRequest) throws ResponseException {
+        var request = buildRequest("POST", "/user", regRequest);
         var response = sendRequest(request);
-        return handleResponse(response, Pet.class);
+        return handleResponse(response, RegAndLoginResult.class);
     }
-
-    public void deletePet(int id) throws ResponseException {
-        var path = String.format("/pet/%s", id);
-        var request = buildRequest("DELETE", path, null);
+    public RegAndLoginResult login(LoginRequest loginRequest) throws ResponseException {
+        var request = buildRequest("POST", "/session", loginRequest);
         var response = sendRequest(request);
-        handleResponse(response, null);
+        return handleResponse(response, RegAndLoginResult.class);
     }
-
-    public void deleteAllPets() throws ResponseException {
-        var request = buildRequest("DELETE", "/pet", null);
+    public void logout(LogoutRequest logoutRequest) throws ResponseException {
+        var request = buildRequest("DELETE", "/session", logoutRequest);
         sendRequest(request);
     }
-
-    public PetList listPets() throws ResponseException {
-        var request = buildRequest("GET", "/pet", null);
+    public ListGamesResult listGames() throws ResponseException {
+        var request = buildRequest("GET", "/game", null);
         var response = sendRequest(request);
-        return handleResponse(response, PetList.class);
+        return handleResponse(response, ListGamesResult.class);
+    }
+    public void createGame(CreateGameRequest createGameRequest) throws ResponseException {
+        var request = buildRequest("POST", "/game", createGameRequest);
+        sendRequest(request);
+
+    }
+    public void joinGame(JoinGameRequest joinGameRequest) throws ResponseException {
+        var request = buildRequest("PUT", "/game", joinGameRequest);
+        sendRequest(request);
+    }
+    public void clear() throws ResponseException {
+        var request = buildRequest("DELETE", "/db", null);
+        sendRequest(request);
     }
 
     private HttpRequest buildRequest(String method, String path, Object body) {
@@ -52,17 +61,17 @@ public class ServerFacade {
         return request.build();
     }
 
-    private BodyPublisher makeRequestBody(Object request) {
+    private HttpRequest.BodyPublisher makeRequestBody(Object request) {
         if (request != null) {
-            return BodyPublishers.ofString(new Gson().toJson(request));
+            return HttpRequest.BodyPublishers.ofString(new Gson().toJson(request));
         } else {
-            return BodyPublishers.noBody();
+            return HttpRequest.BodyPublishers.noBody();
         }
     }
 
     private HttpResponse<String> sendRequest(HttpRequest request) throws ResponseException {
         try {
-            return client.send(request, BodyHandlers.ofString());
+            return client.send(request, HttpResponse.BodyHandlers.ofString());
         } catch (Exception ex) {
             throw new ResponseException(ResponseException.Code.ServerError, ex.getMessage());
         }
@@ -89,4 +98,7 @@ public class ServerFacade {
     private boolean isSuccessful(int status) {
         return status / 100 == 2;
     }
+
 }
+
+
--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -2,18 +2,26 @@ package client;
 
 import org.junit.jupiter.api.*;
 import server.Server;
+import server.ServerFacade;
+import service.RegAndLoginResult;
+import service.RegisterRequest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
-//        facade = new ServerFacade(port);
+//        facade = new server.ServerFacade(String.format("localhost:%d", port));
+//        Not sure if I need the http://, so I have this here just in case I want to switch it real quick
+        facade = new ServerFacade(String.format("http://localhost:%d", port));
     }
 
     @AfterAll
@@ -23,8 +31,14 @@ public class ServerFacadeTests {
 
 
     @Test
+    void validRegister() throws Exception {
+        RegAndLoginResult authData = facade.register(new RegisterRequest("player1", "password", "p1@email.com"));
+        assertTrue(authData.authToken().length() > 10);
+    }
+
+    @Test
     public void sampleTest() {
-        Assertions.assertTrue(true);
+        assertTrue(true);
     }
 
 }
